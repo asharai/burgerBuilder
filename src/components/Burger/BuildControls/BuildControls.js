7a37d90326c88ccd9314a5f379d2808f9264fcde
Arkadii <Seril21@ya.ru>
2020-08-11 16:29:03 +0700

    Add logic for build burger

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl'
+const controls = [
+    {label:'Salad',type:'salad'},
+    {label:'Bacon',type:'bacon'},
+    {label:'Meat',type:'meat'},
+    {label:'Cheese',type:'cheese'},
+
+];
+const buildControls = (props)=>(
+    <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+        {controls.map(ctrl=>(
+            <BuildControl key={ctrl.label}
+                          label={ctrl.label}
+                          added={()=>props.ingredientAdded(ctrl.type)}
+                          remove={()=>props.ingredientRemove(ctrl.type)}
+                          disabled={props.disabled[ctrl.type]}
+            />))}
+    </div>
+)
+export default  buildControls;
