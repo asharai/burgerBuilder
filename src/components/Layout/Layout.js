af3dc46029ee938a22c397dcfef1dc932e45f609
Arkadii <asharaev210497@gmail.com>
2020-08-13 18:43:19 +0700

    add styles for mobile and menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Auxillary from '../../hoc/Auxillary';
-import classes from './Layout.css'
-const layout = (props)=>{
-    return(
-        <Auxillary>
-    <div>Toolbar,SideDrawer,Backdrop</div>
-    <main className={classes.Content}>
-        {props.children}
-    </main>
-    </Auxillary>
-    )
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+
+class Layout extends Component {
+    state={
+        showSideDrawer:false
+    }
+ sideDrawerClosedHandler = ()=>{
+     this.setState({showSideDrawer:false})
+ }
+ sideDrawerToggleHandler = ()=>{
+        this.setState((prevState=>{
+            return{ showSideDrawer: !prevState.showSideDrawer}
+        }))
+    }
+
+
+    render() {
+        return (
+            <Auxillary>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxillary>
+        )
+    }
 }
-export default layout;
+
+export default Layout;
