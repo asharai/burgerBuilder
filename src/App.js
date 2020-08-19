8d9e76819cfc651bedeaa51ff1f18c9d77817bff
Arkadii <asharaev210497@gmail.com>
2020-08-19 17:27:13 +0700

    Add Checkout Component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import styles from './App.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-
+import Checkout from "./containers/Checkout/Checkout";
 class App extends Component {
 
 
@@ -13,6 +13,7 @@ class App extends Component {
       <div className={styles.app}>
       <Layout>
       <BurgerBuilder/>
+      <Checkout/>
       </Layout>
       </div>
     );
