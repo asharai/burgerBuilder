b11f0e9e71e2290ac34232c1debdf93551eda085
Arkadii <asharaev210497@gmail.com>
2020-08-20 10:57:32 +0700

    add logic for routing on checkout page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import styles from './App.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from "./containers/Checkout/Checkout";
-import {BrowserRouter} from "react-router-dom";
+
 import {Route,Switch} from "react-router-dom";
 
 class App extends Component {
@@ -13,19 +13,20 @@ class App extends Component {
 
   render() {
     return (
-        <BrowserRouter >
+
       <div className={styles.app}>
       <Layout>
       <Switch>
-        <Route path='/' exact component={BurgerBuilder}/>
         <Route path='/checkout' component={Checkout}/>
+        <Route path='/' exact component={BurgerBuilder}/>
+
       </Switch>
 
       {/*<BurgerBuilder/>*/}
       {/*<Checkout/>*/}
       </Layout>
       </div>
-        </BrowserRouter>
+
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+import {BrowserRouter} from "react-router-dom";
+const app = (
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+)
 ReactDOM.render(
-  <App />,
+    app,
   
   document.getElementById('root')
 );
