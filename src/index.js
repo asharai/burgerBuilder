d7f46b81f9592d558c35520f2425e65f06a68f88
Arkadii <asharaev210497@gmail.com>
2020-09-06 12:35:26 +0700

    change logic for add and remove ingredients

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
+import reducer from "./store/reducer";
+
+const store = createStore(reducer);
 const app = (
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+   <Provider store = {store}>
+       <BrowserRouter>
+           <App/>
+       </BrowserRouter>
+   </Provider>
 )
 ReactDOM.render(
     app,
